9b4c6c83b009f231b28896a1cba3c3961380eb5d
בת שבע גרינוולד <BatshevaGr@smg.local>
2025-04-30 14:12:06 +0300

    fix app

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 
 const mongoose = require('mongoose')
 const connectDB = async () => {
-try {
-await mongoose.connect(process.env.DATABASE_URI)
-} catch (err) {
-console.error("*****error connection to DB****\n" + err)
-}
+    try {
+        await mongoose.connect(process.env.DATABASE_URI)
+    } catch (err) {
+        console.error("*****error connection to DB****\n" + err)
+    }
 }
 module.exports = connectDB
