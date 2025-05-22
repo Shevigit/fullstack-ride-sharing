cc6f48054f43e9f5de653a71593ac167eb352480
Tzipi-dev <tzipi3135@gmail.com>
2025-05-22 20:23:05 +0300

    design second nav - when there is allready user in localStorage

---
+++ client/src/components/HomePageNav.tsx
@@ -0,0 +1,42 @@
+import { navStyle } from '../CSS/nav'
+import { NavLink } from 'react-router'
+import { NavbarLink, NavbarLinkHover } from '../CSS/homePageNav'
+import { styled } from '@mui/material'
+const HomePageNav = () => {
+    return (
+        <div>
+            <nav style={navStyle}>
+                <div>
+                    <NavLink to='/' >RideShare</NavLink>
+                </div>
+                <NavLink to="/Update" >
+                    <div  style={NavbarLink} >
+                        עדכון נסיעה
+                    </div>
+                </NavLink>
+                <NavLink to="/Offer">
+                    <div style={NavbarLink}>
+                        הצעת נסיעה
+                    </div>
+                </NavLink>
+                <NavLink to="/SearchDrive">
+                    <div style={NavbarLink}>
+                        חיפוש נסיעה
+                    </div>
+                </NavLink>
+                <NavLink to="/About">
+                    <div style={NavbarLink}>
+                        אודות
+                    </div>
+                </NavLink>
+                <NavLink to="/">
+                    <div style={NavbarLink}>
+                        עמוד הבית
+                    </div>
+                </NavLink>
+            </nav>
+        </div>
+    )
+}
+
+export default HomePageNav
