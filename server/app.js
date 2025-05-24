bbd66de063102faa9624fbcdbb3bce8b4911b9ed
sara <sara0527656182@gmail.com>
2025-05-24 22:13:07 +0300

    add feature to search city

--- server/app.js
+++ server/app.js
@@ -12,6 +12,7 @@ const passport = require('passport');
 // const corsOptions = require("./config/corsOptions"); 
 const connectDB = require("./config/db"); 
 const authRoutes = require('./routes/authRoute'); 
+const { log } = require('console');
 // app.use(cors(corsOptions));
 
 const app = express();
@@ -32,18 +33,31 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 app.use('/api', authRoutes); // משתמש ב-authRoutes שיובא למעלה
-app.all('*', (req, res) => {
-    res.status(404);
-    if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'views', '404.html')); // אם יש לך קובץ 404 HTML
-    } else if (req.accepts('json')) {
-        res.json({ message: '404 Not Found' });
-    } else {
-        res.type('txt').send('404 Not Found');
-    }
-});
 
 
+
+
+
+
+const loadCities=()=> {
+  return new Promise((resolve, reject) => {
+    const results = new Set();
+    const filePath = path.join(__dirname, "data", "cities.csv");
+
+    fs.createReadStream(filePath)
+      .pipe(iconv.decodeStream("utf-8"))
+      .pipe(csv())
+      .on("data", (row) => {
+        const city = row["cities"];
+         console.log(row.cities);
+        if (city) results.add(city.trim());
+      })
+      .on("end", () => {
+        resolve(Array.from(results));
+      })
+      .on("error", reject);
+  });
+}
 
 
 
@@ -51,24 +65,28 @@ app.all('*', (req, res) => {
 // מאפשר CORS לבקשות מהדפדפן (חשוב אם ה־frontend בפורט אחר)
 
 // פונקציה לטעינת ערים מתוך קובץ CSV
-const loadCities = () => {
-  return new Promise((resolve, reject) => {
-    const results = new Set();
+// const loadCities = () => {
+//   return new Promise((resolve, reject) => {
+//     const results = new Set();
 
-    fs.createReadStream(path.join(__dirname, "data", "cities.csv"))
-      .pipe(iconv.decodeStream("win1255"))
-      .pipe(csv())
-      .on("data", (row) => {
-        const city = row["שם_ישוב"];
-        if (city) results.add(city);
-      })
-      .on("end", () => {
-        console.log("CSV loaded. Total cities:", results.size);
-        resolve([...results]);
-      })
-      .on("error", reject);
-  });
-};
+//     fs.createReadStream(path.join(__dirname, "data", "cities.csv"))
+//       .pipe(iconv.decodeStream("win1255"))
+//       .pipe(csv())
+//       .on("data", (city) => {
+//         // const city = row["שם_ישוב"];
+//         // console.log(row.שם_ישוב);
+        
+//         if (city) results.add(city);
+//       })
+//       .on("end", () => {
+//         console.log("CSV loaded. Total cities:", results.size);
+//         console.log(results);
+        
+//         resolve([...results]);
+//       })
+//       .on("error", reject);
+//   });
+// };
 
 // Route שמחזיר את רשימת הערים
 app.get("/api/cities", async (req, res) => {
@@ -98,3 +116,20 @@ mongoose.connection.on('error', err => {
     console.error("MongoDB connection error:", err); // השתמש ב-console.error
     // כאן אתה יכול להוסיף לוגיקה נוספת לטיפול בשגיאות קריטיות
 });
+
+
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html')); // אם יש לך קובץ 404 HTML
+    } else if (req.accepts('json')) {
+        res.json({ message: '404 Not Found' });
+    } else {
+        res.type('txt').send('404 Not Found');
+    }
+});
+
+
+
+
+
